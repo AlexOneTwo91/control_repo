e6ccf7574b02e292d7295d492689283018b0b135
AlexOneTwo91 <93661252+AlexOneTwo91@users.noreply.github.com>
2021-11-05 10:15:24 +0200

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh)server { 
+        package {'openssh-server':
+                ensure => present,
+        }
+        service { 'sshd':
+                ensure => 'running',
+                enable => 'true',
+        }
+        ssh_authorized_key { 'root@master.puppet.vm':
+                ensure => present,
+                user   => 'root',
+                type   => "ssh-rsa,
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCvvZW6Hnzlf2/QWePdbBsYrmfq4BiwZaP+A24mHCCAu7zktlmh+aP2L8KhEm/EKJcyoFKcSO2Dc6BTdQZhJxJYn3tS6bTH8jTz9OSEpCB25UUTMelvdaHH6uvZc+ToAsEJ6kiwFLYX09x29dyeKRQSegllBCVATqJoVZbg8lqa+tgqFAHtKIpDWmfOY/MSNifKhEF8Qb3YbeDP12ZAPnI6DeoBHmNRE37e7AnEyZMoLImSicF8ZVB1NtgHWbqWmnTeqKJ+DIZkzh691It8370/DXGbnlb63mvZta66n7mp0HmXQ5VTnHtmcrayI1i54CLAM2Oy6D/w14DrFZJAr2L7',
+        }
+}        
