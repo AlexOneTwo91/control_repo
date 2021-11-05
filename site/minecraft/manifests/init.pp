a0bf317cf2444cb8300dec9f23f4215460b70f54
AlexOneTwo91 <93661252+AlexOneTwo91@users.noreply.github.com>
2021-11-05 11:59:01 +0200

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,16 @@
+class minecraft {
+  file {'/opt/minecraft/':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/systemd/system/minecraft.service':
+    ensure => file
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
